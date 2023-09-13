c9ccbdb09ed74f0c30d4d5afbdae5433b2f54f23
Jan Heiland <jnhlnd@gmail.com>
2023-09-13 21:26:06 +0200

    and no local reveal

--- mkdc.sh
+++ mkdc.sh
@@ -4,7 +4,6 @@ pandoc $MDFILE -o index.html \
     --citeproc \
     --mathjax  \
     -t revealjs --slide-level=2 -s \
-    -V revealjs-url=reveal.js \
     -V theme=solarized \
     -V NavigationMode=linear \
     -V viewDistance=15 -V width=1280 -V height=880 -V margin=0.05
